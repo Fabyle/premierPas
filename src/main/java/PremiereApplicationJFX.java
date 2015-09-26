8537aca7b1511ed1c534754fdcdba97d2a8d4ccc
fabien <fabien99@gmail.com>
2015-09-26 18:16:22 +0200

    premier commit

---
+++ src/main/java/PremiereApplicationJFX.java
@@ -0,0 +1,18 @@
+/**
+ * Created by fabien on 26/09/2015.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PremiereApplicationJFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
